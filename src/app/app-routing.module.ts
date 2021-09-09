0f05bc807bd19de39e08a6549b4ae603a27753c2
Braulio Talamante <Braulio.Talamante@softrax.com>
2021-09-08 21:39:06 -0500

    Commit12

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    /**
+     * Clave del lazyload
+     * Cuando alguien entre al auth path carga sus hijos y esos hijos vienen del producto de auth.module
+     * Cuando se cargue en memoria por la promesa el modulo que se carga es el AuthModule
+     */
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    // component: ErrorPageComponent
+    redirectTo: '404'
+  }
+]
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Heroe } from '../interfaces/heroe';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private baseUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroePorId(id: string): Observable<Heroe> {
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
+  }
+}
