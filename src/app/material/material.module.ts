1b68b105d3354e6a98118a7a4aac5e70e6d5ba87
Braulio Talamante <Braulio.Talamante@softrax.com>
2021-09-09 19:52:47 -0500

    Commit

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -20,7 +22,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatListModule,
     MatCardModule,
     MatProgressSpinnerModule,
-    MatGridListModule
+    MatGridListModule,
+    MatAutocompleteModule,
+    MatFormField
   ]
 })
 export class MaterialModule { }
